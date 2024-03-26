c3b6046ea5117e5804987eea87f991e3180b6b2e
RajrupDasid <rajrupofficialid@protonmail.com>
2024-03-26 12:23:21 +0530

    footer banner added
    some schema updated
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ src/api/footer-banner/routes/footer-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footer-banner router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::footer-banner.footer-banner');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -802,6 +802,11 @@ export interface ApiCategoryCategory extends Schema.CollectionType {
     name: Attribute.String;
     icon: Attribute.Media;
     color: Attribute.String;
+    products: Attribute.Relation<
+      'api::category.category',
+      'manyToMany',
+      'api::product.product'
+    >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -813,6 +818,78 @@ export interface ApiCategoryCategory extends Schema.CollectionType {
       Attribute.Private;
     updatedBy: Attribute.Relation<
       'api::category.category',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiFooterBannerFooterBanner extends Schema.CollectionType {
+  collectionName: 'footer_banners';
+  info: {
+    singularName: 'footer-banner';
+    pluralName: 'footer-banners';
+    displayName: 'Footer Banner';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    images: Attribute.Media;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::footer-banner.footer-banner',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::footer-banner.footer-banner',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiProductProduct extends Schema.CollectionType {
+  collectionName: 'products';
+  info: {
+    singularName: 'product';
+    pluralName: 'products';
+    displayName: 'Product';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    name: Attribute.String;
+    description: Attribute.Text;
+    mrp: Attribute.Float;
+    sellingPrice: Attribute.Float;
+    itemQuantityType: Attribute.String;
+    slug: Attribute.UID<'api::product.product', 'name'>;
+    images: Attribute.Media;
+    categories: Attribute.Relation<
+      'api::product.product',
+      'manyToMany',
+      'api::category.category'
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::product.product',
       'oneToOne',
       'admin::user'
     > &
@@ -871,6 +948,8 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'api::category.category': ApiCategoryCategory;
+      'api::footer-banner.footer-banner': ApiFooterBannerFooterBanner;
+      'api::product.product': ApiProductProduct;
       'api::slider.slider': ApiSliderSlider;
     }
   }
